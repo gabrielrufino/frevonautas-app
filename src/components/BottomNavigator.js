f64c5eae433cc0175f60e459a09dab9a7eb570eb
Gabriel Rufino <contato@gabrielrufino.com>
2020-10-04 10:53:30 -0300

    Initial bottom navigator

---
+++ src/components/BottomNavigator.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Row, Col } from 'react-bootstrap'
+import { faBookOpen, faGamepad, faBell, faCog } from '@fortawesome/free-solid-svg-icons'
+
+import './BottomNavigator.css'
+
+export default function BottomNavigator() {
+  return (
+    <Row className="bottom-navigator fixed-bottom text-light text-center">
+      <Col>
+        <FontAwesomeIcon size="lg" icon={faBookOpen} />
+      </Col>
+      <Col>
+        <FontAwesomeIcon size="lg" icon={faGamepad} />
+      </Col>
+      <Col>
+        <FontAwesomeIcon size="lg" icon={faBell} />
+      </Col>
+      <Col>
+        <FontAwesomeIcon size="lg" icon={faCog} />
+      </Col>
+    </Row>
+  )
+}
