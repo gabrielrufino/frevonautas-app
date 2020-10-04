520cdf61b6f1ee3ea079c525404a9a02fc0a1420
Gabriel Rufino <contato@gabrielrufino.com>
2020-10-04 16:54:00 -0300

    Notifications

--- src/views/Notifications.js
+++ src/views/Notifications.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Badge, Container } from 'react-bootstrap'
 
 import Header from '../components/Header'
 import BottomNavigator from '../components/BottomNavigator'
 
 export default function Notifications() {
+  const [notifications, setNotifications] = useState([
+    {
+      id: 1,
+      text: 'Porque existe vida na terra?'
+    }
+  ])
+
   return (
     <>
       <Header />
-
       <div className="mt-5 pt-5" />
+
       <Container>
-        Notificações
+        {notifications.map(notification => (
+          <Badge className="w-100 py-4 text-left" variant="secondary">
+            {notification.text}
+          </Badge>
+        ))}
       </Container>
 
       <BottomNavigator selected="notifications" />
